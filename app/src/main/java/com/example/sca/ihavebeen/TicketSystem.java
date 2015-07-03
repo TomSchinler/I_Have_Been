23860bb937c16ef852d6ef53de15f4a62edbba97
Tom Schinler <t.schinler88@gmail.com>
2015-07-03 13:46:26 -0500

    Create ticketing system

---
+++ app/src/main/java/com/example/sca/ihavebeen/TicketSystem.java
@@ -0,0 +1,32 @@
+package com.example.sca.ihavebeen;
+
+/**
+ * Created by Tom Schinler on 7/3/2015.
+ */
+public class TicketSystem {
+
+    public int getTickets() {
+        return tickets;
+    }
+
+    public void setTickets(int tickets) {
+        this.tickets = tickets;
+    }
+
+    // When Parse is set up get tickets from Parse User
+    private int tickets = 25;
+
+    // Costs 4 tickets to play the game
+    public int costOfGame() {
+        tickets = tickets - 4;
+        return tickets;
+    }
+
+    // Winner receives 8 tickets (game cost plus 4)
+    public int sweetTasteOfVictory() {
+        tickets = tickets + 8;
+        return tickets;
+    }
+
+    // For Premium version set up betting methods here
+}
