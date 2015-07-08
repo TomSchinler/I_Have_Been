79c89d2c9422f5d82dbed407295b733b9ee7a1c9
Tom Schinler <t.schinler88@gmail.com>
2015-07-08 08:55:41 -0500

    added activity templates to begin building around game activity

--- app/src/main/java/com/example/sca/ihavebeen/NextRound.java
+++ app/src/main/java/com/example/sca/ihavebeen/NextRound.java
@@ -4,24 +4,20 @@ import com.example.sca.ihavebeen.util.SystemUiHider;
 
 import android.annotation.TargetApi;
 import android.app.Activity;
-import android.content.Context;
-import android.database.Cursor;
 import android.os.Build;
 import android.os.Bundle;
-import android.os.CountDownTimer;
 import android.os.Handler;
-import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.AutoCompleteTextView;
-import android.widget.Button;
-import android.widget.ProgressBar;
-import android.widget.TextView;
-import android.widget.Toast;
 
 
-public class GameActivity extends Activity {
+/**
+ * An example full-screen activity that shows and hides the system UI (i.e.
+ * status bar and navigation/system bar) with user interaction.
+ *
+ * @see SystemUiHider
+ */
+public class NextRound extends Activity {
     /**
      * Whether or not the system UI should be auto-hidden after
      * {@link #AUTO_HIDE_DELAY_MILLIS} milliseconds.
@@ -50,33 +46,11 @@ public class GameActivity extends Activity {
      */
     private SystemUiHider mSystemUiHider;
 
-
-    
-
-    private GameDatabase db;
-
-    //member variables
-    ProgressBar mProgressBar;
-    TextView mHardClue;
-    TextView mMediumClue;
-    TextView mEasy1Clue;
-    TextView mEasy2Clue;
-    TextView mGiveAwayClue;
-    String mActorName;
-    String[] mGuessAC;
-    AutoCompleteTextView mUserGuess;
-
-
-
-
-
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.activity_game);
+        setContentView(R.layout.activity_next_round);
 
         final View controlsView = findViewById(R.id.fullscreen_content_controls);
         final View contentView = findViewById(R.id.fullscreen_content);
@@ -135,122 +109,10 @@ public class GameActivity extends Activity {
             }
         });
 
-
-        //Get Text Views
-        mProgressBar = (ProgressBar) findViewById(R.id.gameTimer);
-        mHardClue = (TextView)findViewById(R.id.hardClueTextView);
-        mMediumClue = (TextView)findViewById(R.id.mediumClueTextView);
-        mEasy1Clue = (TextView)findViewById(R.id.easy1ClueTextView);
-        mEasy2Clue = (TextView)findViewById(R.id.easy2ClueTextView);
-        mGiveAwayClue = (TextView)findViewById(R.id.giveAwayClueTextView);
-        mUserGuess = (AutoCompleteTextView)findViewById(R.id.userGuessBox);
-
-
-
-        //Set AutoComplete Array Adapter
-        mGuessAC = getResources().getStringArray(R.array.guessAC);
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>
-                (this, android.R.layout.select_dialog_item, mGuessAC);
-        mUserGuess.setAdapter(adapter);
-        mUserGuess.setThreshold(10);
-
-        String hardClue = "";
-        String mediumClue = "";
-        String easy1Clue = "";
-        String easy2Clue = "";
-        String giveAwayClue = "";
-        String actorName = "";
-
-        db = new GameDatabase(this);
-
-        Cursor cursor = db.getActorsFromDB();
-        try {
-            while (cursor.moveToNext()) {
-                actorName = cursor.getString(0);
-                hardClue = cursor.getString(1);
-                mediumClue = cursor.getString(2);
-                easy1Clue = cursor.getString(3);
-                easy2Clue = cursor.getString(4);
-                giveAwayClue = cursor.getString(5);
-            }
-        }
-        finally {
-            cursor.close();
-        }
-        db.close();
-
-        mHardClue.setText(hardClue);
-        mMediumClue.setText(mediumClue);
-        mEasy1Clue.setText(easy1Clue);
-        mEasy2Clue.setText(easy2Clue);
-        mGiveAwayClue.setText(giveAwayClue);
-        mActorName = actorName;
-        Log.v("actor is", actorName);
-
-        Button button = (Button)findViewById(R.id.gameSubmitButton);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //Check if guess is correct
-                String answer = mUserGuess.getText().toString();
-                String actorName = mActorName;
-                Context context = getApplicationContext();
-
-                if(answer.equalsIgnoreCase(actorName)) {
-                    //Toast is for testing only... Create Intent for next activity if correct
-                    Toast toast = Toast.makeText(context, "That's Right", Toast.LENGTH_LONG);
-                    toast.show();
-                }
-                else {
-                    //toast is for testing only
-                    Toast toast = Toast.makeText(context, "You Done Fucked Up A-Aron", Toast.LENGTH_LONG);
-                    toast.show();
-                }
-            }
-        });
-
-
-
-        //Start timer onCreate after DB call
-        mProgressBar.setProgress(0);
-
-        final int totalMsecs = 90 * 1000; // 90 seconds in milli seconds
-        int callInterval = 10;
-
-        /** CountDownTimer */
-        new CountDownTimer(totalMsecs, callInterval) {
-
-            public void onTick(long millisUntilFinished) {
-
-                int secondsRemaining = (int) millisUntilFinished / 1000;
-
-                float fraction = millisUntilFinished / (float) totalMsecs;
-
-            // progress bar is based on scale of 1 to 100;
-                mProgressBar.setProgress ( (int) (fraction * 100) );
-
-                if (secondsRemaining <= 80) {
-                   mMediumClue.setVisibility(View.VISIBLE);
-                }
-                if (secondsRemaining <= 70) {
-                    mEasy1Clue.setVisibility(View.VISIBLE);
-                }
-                if (secondsRemaining <= 60) {
-                    mEasy2Clue.setVisibility(View.VISIBLE);
-                }
-            }
-
-            public void onFinish() {
-                Log.d("LOG_tag", ">>>>>>>>> countdown timer on finish");
-            }
-        }.start();
-
-
-
-
-
-
-
+        // Upon interacting with UI controls, delay any scheduled hide()
+        // operations to prevent the jarring behavior of controls going away
+        // while interacting with the UI.
+        findViewById(R.id.dummy_button).setOnTouchListener(mDelayHideTouchListener);
     }
 
     @Override
@@ -295,12 +157,4 @@ public class GameActivity extends Activity {
         mHideHandler.removeCallbacks(mHideRunnable);
         mHideHandler.postDelayed(mHideRunnable, delayMillis);
     }
-
-
-
-
-
-
-
-
 }
